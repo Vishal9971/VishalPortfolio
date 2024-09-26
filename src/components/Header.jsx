852e276ffb5ac2f12186d5e2ce1aa3456dae1c30
Vishal Sharma <87796097+Vishal9971@users.noreply.github.com>
2024-09-26 10:19:39 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,17 +10,17 @@ function Header() {
             Vishal Sharma
           </h2>
           <h3 className="text-3xl font-thin">Student &#10242; Developer &#10242; Quick Learner </h3>
-          <div className="flex flex-wrap flex-row text-l text-white gap-36 m-auto py-3">
-            <Link to="/" className="hover:text-red-600 hover:text-2xl">
+          <div className="w-full flex flex-wrap flex-row text-l text-white justify-evenly m-auto py-3 headerMedia">
+            <Link to="/" className="hover:text-red-600 hover:text-2xl Link">
               Home
             </Link>
-            <Link to="/project" className="hover:text-red-600 hover:text-2xl">
+            <Link to="/project" className="hover:text-red-600 hover:text-2xl Link">
               Project
             </Link>
-            <Link to="/contact" className="hover:text-red-600 hover:text-2xl">
+            <Link to="/contact" className="hover:text-red-600 hover:text-2xl Link">
               Contact
             </Link>
-            <Link to="/resume" className="hover:text-red-600 hover:text-2xl">
+            <Link to="/resume" className="hover:text-red-600 hover:text-2xl Link">
               Resume
             </Link>
           </div>
